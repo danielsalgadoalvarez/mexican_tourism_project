afdda9358b9c3a755e6f33291ee9ee221c84c006
Daniel Salgado-Alvarez <dsalgadoalvarez@college.harvard.edu>
2020-11-05 17:15:22 -0500

    first

---
+++ tourism_project_app/app.R
@@ -0,0 +1,92 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+revenue <- readRDS("revenue.RDS")
+visits <- readRDS("visits.RDS")
+
+# Define UI for application that draws a histogram
+ui <- navbarPage(
+    "International Tourism in Mexico",
+    tabPanel("Size of Tourism",
+             fluidPage(
+                 titlePanel("How Big is the Tourism Sector?"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         radioButtons(
+                             "buttons",
+                             "Interactive Features (Coming Soon)",
+                             c("Option A" = "a", "Option B" = "b"))
+                         ),
+                     mainPanel(plotOutput("revPlot"))
+                     )
+                 ),
+             mainPanel(plotOutput("visPlot"))
+             ),
+    tabPanel("Factors of Tourism",
+             fluidPage(
+                 titlePanel("What Factors Influence Tourism Revenue?"),
+                 mainPanel(plotOutput("factPlot"))
+             )
+    ), 
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Growing up, I used to travel to Mexico with my parents to visit 
+               family members. While most of my time there was spent at my 
+               grandparent's ranch, I did occasionaly travel to other cities
+               and towns to see musuems or go shopping. Prior to the COVID-19
+               pandemic, my mother and I were planning a trip to Mexico for the
+               sole purpose of being tourists in my mother's country. Since that
+               trip was cancelled, I have been interested in learning more about
+               the tourism sector of Mexico. With this project, I want to learn 
+               who travels to Mexico, where they go to spend their money, why 
+               people travel, and who benefits from the tourist economy"),
+             h3("About Me"),
+             p("My name is Daniel Salgado-Alvarez and I am freshman at Harvard 
+             College. I study Government on the Data Science track. 
+             You can reach me at dsalgadoalvarez@college.harvard.edu."),
+             h3("Repo"),
+             p("https://github.com/danielsalgadoalvarez/final_project")))
+
+server <- function(input, output) {
+    
+    output$revPlot <- renderPlot({
+        ggplot(revenue, aes(x = year, y = revenue, color = reason)) +
+            geom_line(na.rm = TRUE) +
+            labs(title = "Total Revenue from International Tourism by Reason 
+                 for Trip",
+                 x = "Year",
+                 y = "Revenue in Millions of Dollars")})  
+   
+        output$visPlot <- renderPlot({
+            ggplot(visits, aes(x = year, y = quantity, color = tourist_type)) +
+                geom_line(na.rm = TRUE) +
+                labs(title = "Total Number of International Tourists by Distinction",
+                     x = "Year",
+                     y = "Number of Tourists")      
+    })
+        
+        output$factPlot <- renderPlot({
+            ggplot(full, aes(x = victimization, y = GDP_capita)) +
+                geom_point(na.rm = TRUE) +
+                geom_smooth() +
+                labs(title = "Tourism GDP",
+                     x = "Tourism GDP Per Capita",
+                     y = "Victimization Per 100,000")      
+        })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
