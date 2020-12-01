39a1d307597d4af9ddba7e9543a3271b7ded3be1
Daniel Salgado-Alvarez <dsalgadoalvarez@college.harvard.edu>
2020-11-30 21:50:35 -0600

    pre-submit for milestone 8

--- tourism_project_app/app.R
+++ tourism_project_app/app.R
@@ -9,9 +9,10 @@
 
 library(shiny)
 library(tidyverse)
+library(readxl)
 revenue <- readRDS("revenue.RDS")
 visits <- readRDS("Visits.RDS")
-full <- readRDS("test.RDS")
+full <- readRDS("end.RDS")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
@@ -25,8 +26,14 @@ ui <- navbarPage(
                                                  choices = unique(revenue$reason), 
                                                  selected = "All"),
                               plotOutput("revPlot")), 
-                     tabPanel("Visits", plotOutput("visPlot"))
-                    
+                   
+                     tabPanel("Visits", 
+                              checkboxGroupInput("vis", 
+                                                 label = "Select Type", 
+                                                 choices = unique(visits$tourist_type), 
+                                                 selected = "International Visitors"),
+                              plotOutput("visPlot"))
+                      
                  )
              )), 
              
@@ -34,31 +41,70 @@ ui <- navbarPage(
     tabPanel("Factors of Tourism",
              fluidPage(
                  titlePanel("What Factors Influence Tourism Revenue?"),
-                 selectInput("factor", label = "Select Factor", choices = c("victimization", "score")),
-                 mainPanel(plotOutput("factPlot"))
+                 tabsetPanel(
+                     tabPanel("Plot",
+                 selectInput("factor", label = "Select Factor", 
+                             choices = c("victimization", "score", "one_star",
+                                         "two_star", "three_star", "four_star",
+                                         "five_star", "luxury_percent", 
+                                         "luxury_ratio",
+                                         "resturants", "nightlife", 
+                                         "nightlife_percent", 
+                                         "nightlife_ratio", "agencies", 
+                                         "schools")),
+                 mainPanel(plotOutput("factPlot"))),
+                 tabPanel("Factor Definitions",
+                          h3("Factors"),
+                          p("victimization: "),
+                          p("score:"),
+                          p("one_star:"),
+                          p("two_star:"),
+                          p("three_star:"),
+                          p("four_star:"),
+                          p("five_star:"),
+                          p("luxury_percent:"),
+                          p("luxury_ratio:"),
+                          p("nightlife:"),
+                          p("nightlife_percent:"),
+                          p("nightlife_ratio:"),
+                          p("agencies:"),
+                          p("schools:"))
+             )
              )
     ), 
-    tabPanel("Discussion",
-             titlePanel("Discussion Title"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
+    tabPanel("Model",
+             titlePanel("Model Discussion"),
+             p("Here is the predictive model I created:")),
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
              p("Growing up, I used to travel to Mexico with my parents to visit 
                family members. While most of my time there was spent at my 
                grandparent's ranch, I did occasionaly travel to other cities
-               and towns to see musuems or go shopping. Prior to the COVID-19
+               and towns to see museums or go shopping. Prior to the COVID-19
                pandemic, my mother and I were planning a trip to Mexico for the
                sole purpose of being tourists in my mother's country. Since that
                trip was cancelled, I have been interested in learning more about
                the tourism sector of Mexico. With this project, I want to learn 
                who travels to Mexico, where they go to spend their money, why 
-               people travel, and who benefits from the tourist economy"),
+               people travel, and what tourist attractions generate the most 
+               revenue"),
              h3("About Me"),
              p("My name is Daniel Salgado-Alvarez and I am freshman at Harvard 
              College. I study Government on the Data Science track. 
              You can reach me at dsalgadoalvarez@college.harvard.edu."),
+             h3("Data Sources"),
+             p("GDP Data:"),
+             p("https://www.inegi.org.mx/app/tabulados/default.aspx?pr=17&vr=7&in=38&tp=20&wr=1&cno=2"),
+             p("Population Data:"),
+             p("https://www.inegi.org.mx/app/tabulados/interactivos/?pxq=
+             Poblacion_Poblacion_01_903d6ce7-5e37-4828-8060-61d3116aaec5"),
+             p("Victimization Data:"),
+             p("https://www.inegi.org.mx/temas/victimizacion/"),
+             p("Crime Perception Data:"),
+             p("https://www.inegi.org.mx/temas/percepcion/"),
+             p("Hotel, Restaurant, Nightlife, Travel Agency, and Tourism School Data:"),
+             p("https://datatur.sectur.gob.mx/SitePages/CompendioEstadistico.aspx"),
              h3("Repo"),
              p("https://github.com/danielsalgadoalvarez/final_project")))
 
@@ -75,7 +121,9 @@ server <- function(input, output) {
                  y = "Revenue in Millions of Dollars")})  
    
         output$visPlot <- renderPlot({
-            ggplot(visits, aes(x = year, y = quantity, color = tourist_type)) +
+            visits %>%
+                filter(tourist_type %in% input$vis) %>%
+            ggplot(aes(x = year, y = quantity, color = tourist_type)) +
                 geom_line(na.rm = TRUE) +
                 labs(title = "Total Number of International Tourists by Distinction",
                      x = "Year",
